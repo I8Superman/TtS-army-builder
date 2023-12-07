de8436125f614f7cdc9aa6958914c1e7fdbbb06f
Simon A <70197244+I8Superman@users.noreply.github.com>
2023-12-07 14:38:19 +0100

    Root header graphics in place

--- src/pages/CreateArmy/ArmyForm/ArmyForm.jsx
+++ src/pages/CreateArmy/ArmyForm/ArmyForm.jsx
@@ -1,78 +1,94 @@
 
-import { useState } from 'react'
 import './ArmyForm.css'
 import { useLocation } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
+import { DevTool } from '@hookform/devtools'
 
 export default function ArmyForm() {
     const location = useLocation()
     const { list } = location.state
 
-    const rhf = useForm();
-    console.log(rhf);
+    // const [armyData, setArmyData] = useState({
+    //     name: '',
+    //     period: '',
+    //     unitNumber: 0,
+    //     country: ''
+    // })
 
-    const [armyData, setArmyData] = useState({
-        name: '',
-        period: '',
-        unitNumber: 0
-    })
+    const { register, control, handleSubmit, formState } = useForm();
+    const { errors } = formState;
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(armyData);
-        console.log('submitted!');
+    const onSubmit = (formValues) => {
+        console.log('Form submitted', formValues);
     }
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setArmyData((prevData) => ({ ...prevData, [name]: value }))
-        console.log(armyData)
-    }
+    // const handleChange = (e) => {
+    //     const { name, value } = e.target;
+    //     setArmyData((prevData) => ({ ...prevData, [name]: value }))
+    //     console.log(armyData)
+    // }
 
-    const resetForm = () => {
-        setArmyData({
-            name: '',
-            period: '',
-            unitNumber: 0
-        })
-    }
+    // const resetForm = () => {
+    //     setArmyData({
+    //         name: 'name here',
+    //         period: 'time period',
+    //         unitNumber: 0
+    //     })
+    // }
 
     return (
         <div className="army-form page">
             <h3>Create {list} Army List</h3>
-            <div className="army-form page">
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        <span>Army List title:</span>
-                        <input
-                            name="name"
-                            type="text"
-                            onChange={handleChange}
-                            value={armyData.name}
-                        />
-                    </label>
-                    <label>
-                        <span>Time period:</span>
-                        <input
-                            name="period"
-                            type="text"
-                            onChange={handleChange}
-                            value={armyData.period}
-                        />
-                    </label>
-                    <label>
-                        <span># of units:</span>
-                        <input
-                            name="unitNumber"
-                            type="number"
-                            onChange={handleChange}
-                            value={armyData.unitNumber}
-                        />
-                    </label>
-                    <button>Submit form</button>
-                    <button onClick={resetForm}>Reset Form</button>
-                </form>
-            </div>
+            {/* noValidate overrides the html validation so we can make our own */}
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
+                <label>
+                    <span>Army List title:</span>
+                    <input
+                        name="name"
+                        {...register('name', {
+                            required: 'Name is required'
+                        })}
+                    // placeholder="Army name here"
+                    />
+                    <p className='error'>{errors.name?.message}</p>
+                </label>
+                <label>
+                    <span>Time period:</span>
+                    <input
+                        name="period"
+                        {...register('period', {
+                            required: 'A time period is needed!'
+                        })}
+                    // placeholder="Time perod"
+                    />
+                    <p className='error'>{errors.period?.message}</p>
+                </label>
+                <label>
+                    <span># of units:</span>
+                    <input
+                        name="unitNumber"
+                        {...register('unitNumber', {
+                            required: 'also required'
+                        })}
+                    // placeholder="Number of units"
+                    />
+                    <p className='error'>{errors.unitNumber?.message}</p>
+                </label>
+                <label>
+                    <span>State:</span>
+                    <select name="country"
+                        {...register('country')}
+                    >
+                        <option>Sparta</option>
+                        <option>Greece</option>
+                        <option>Persia</option>
+                        <option>France</option>
+                    </select>
+                </label>
+                <button type="submit">Submit form</button>
+                {/* <button onClick={resetForm}>Reset Form</button> */}
+            </form>
+            <DevTool control={control} />
         </div>
     )
 }
--- src/views/RootView/RootView.jsx
+++ src/views/RootView/RootView.jsx
@@ -1,12 +1,21 @@
 
 import './RootView.css'
-import bgImage from '@/assets/svgs/laurels-purple.svg'
-
+import { NavLink } from 'react-router-dom'
 import { Outlet } from 'react-router-dom'
+import arrowLeftWhite from '@/assets/svgs/arrow-left-white.svg'
+import burgerMenuLightBlue from '@/assets/svgs/burger-menu-light-blue.svg'
 
 export default function RootView() {
     return (
         <div className="root view">
+            <header className='header-nav'>
+                <NavLink className='icon go-back header-nav-item'>
+                    <img src={arrowLeftWhite} alt="back-arrow" />
+                </NavLink>
+                <nav className='burger-menu header-nav-item'>
+                    <img src={burgerMenuLightBlue} alt="" />
+                </nav>
+            </header>
             <main>
                 <Outlet />
             </main>
